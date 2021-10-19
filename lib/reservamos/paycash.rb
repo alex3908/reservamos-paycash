699f559b4a3f28d0ac74d5b82c8bef96bc834090
Dimorfa <dianamoreno1503@gmail.com>
2021-10-19 14:59:14 -0500

    first commit

---
+++ lib/reservamos/paycash.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "paycash/version"
+
+module Reservamos
+  module Paycash
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
